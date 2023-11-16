f2af96e137ff3ca8af5b9b9835c6ff69a578a4ed
vien <bvien75@gmail.com>
2023-11-16 21:52:03 +0700

    ThongKe

--- MaiLinhApp/src/main/java/fpt/mailinhapp/repository/DatVeRepository.java
+++ MaiLinhApp/src/main/java/fpt/mailinhapp/repository/DatVeRepository.java
@@ -2,7 +2,20 @@ package fpt.mailinhapp.repository;
 
 import fpt.mailinhapp.domain.DatVe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface DatVeRepository extends JpaRepository<DatVe, Long> {
+    @Query(value = "select sum(d.tongTien) from DatVe d " +
+            "WHERE MONTH (d.ngayDatVe) between ?1 and ?2 and year =?3 ",nativeQuery = true)
+    Long doanhThuTheoQuy(int month1, int month2, int year);
+    @Query(value = "select count(VeXeChiTiet )  from DatVe d " +
+            "WHERE DATE (d.ngayDatVe) between 1 and 30 " +
+            "and MONTH (d.ngayDatVe)=?" +
+            "and Year (d.ngayDatve)=?",nativeQuery = true)
+    Long thongKeTheoThang(int month,int year);
+    @Query(value = "select count (VeXeChiTiet ) from DatVe d " +
+            "WHERE YEAR (d.ngayDatVe) = ?1", nativeQuery = true)
+    Long thongKeTheoNam(int year);
+
 }
--- MaiLinhApp/src/main/java/fpt/mailinhapp/repository/VeXeChiTietRepository.java
+++ MaiLinhApp/src/main/java/fpt/mailinhapp/repository/VeXeChiTietRepository.java
@@ -2,7 +2,27 @@ package fpt.mailinhapp.repository;
 
 import fpt.mailinhapp.domain.VeXeChiTiet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface VeXeChiTietRepository extends JpaRepository<VeXeChiTiet, Long> {
+    @Query(value = "select count(VeXeChiTiet )  from VeXeChiTiet v inner join DatVe d " +
+            "on v.maVe =d.maVe " +
+            "WHERE MONTH (d.ngayDatVe) between ? and ?",nativeQuery = true)
+    Long thongKeTheoQuy(int month1, int month2);
+    @Query(value = "select count(VeXeChiTiet )  from VeXeChiTiet v inner join DatVe d " +
+            "on v.maVe =d.maVe " +
+            "WHERE DATE (d.ngayDatVe) between 1 and 30 " +
+            "and MONTH (d.ngayDatVe)=?" +
+            "and Year (d.ngayDatve)=?",nativeQuery = true)
+    Long thongKeTheoThang(int month,int year);
+
+    @Query(value = "select count (VeXeChiTiet ) from VeXeChiTiet  v inner join DatVe d " +
+            "on v.maVe =d.maVe " +
+            "WHERE YEAR (d.ngayDatVe) = ?1", nativeQuery = true)
+    Long thongKeTheoNam(int year);
+
+
+
+
 }
