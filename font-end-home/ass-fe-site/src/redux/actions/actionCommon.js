8c21fad8f5f94c4a2d821a9771cf8471639a3c38
hoanghuu2011 <huu2813@gmail.com>
2023-11-16 19:07:10 +0700

    demo

--- font-end-home/ass-fe-site/src/redux/actions/actionCommon.js
+++
@@ -1,8 +0,0 @@
-import { COMMON_LOADING_SET } from "./actiontypes";
-
-export const setLoading = (loading) => (dispatch) => {
-  dispatch({
-    type: COMMON_LOADING_SET,
-    payload: loading,
-  });
-};
--- font-end-home/ass-fe-site/src/redux/rootReducer.jsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import AccountReducer from "./reducers/AccountReducer";
-import commonReducer from "./reducers/commonReducer";
-
-const rootReducer = combineReducers({
-  AccountReducer,
-  commonReducer,
-});
-export default rootReducer;
